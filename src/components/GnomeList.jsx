368c56f3c91b00f6313a13e3d8e60f3733ed9eec
Dedicker <dedickerremi@gmail.com>
2020-04-23 01:26:16 +0200

    remove useless import

--- src/components/GnomeList.jsx
+++ src/components/GnomeList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import GnomeListElement from './GnomeListElement';
 import { Row, Col } from 'react-bootstrap';
